2c4c06e893b114d284ee25053c27094d1c7a063f
ruperty <rupert@moonsit.co.uk>
2022-12-10 23:23:47 +0000

    u

---
+++ Libraries/python/pctlocal/tests/ga/pctobject/test-pendulum.py
@@ -0,0 +1,42 @@
+
+
+from pct.environments import Pendulum_1
+from pct.environments import Pendulum
+from pct.environments import CartPoleV1
+
+
+from pct.functions import Constant
+
+
+steps=1
+
+test = 1
+
+if test ==1:
+    # seed 3 circles anti-clockwise
+    pen = Pendulum(render=True, seed=4) 
+    pen.add_link(Constant([1]))
+    print(pen.get_config())
+
+    for i in range(100):
+        pen.run(steps=steps, verbose=True)
+    pen.close()
+
+if test ==2:
+    # seed 3 circles anti-clockwise
+    cp = CartPoleV1(render=True, seed=4) 
+    cp.add_link(Constant(1))
+    print(cp.get_config())
+
+    for i in range(100):
+        cp.run(steps=steps, verbose=True)
+    cp.close()
+
+if test ==3:
+    pen = Pendulum_1(render=True)
+    namespace=pen.namespace
+    pen.add_link(Constant([1], namespace=namespace))
+    print(pen.get_config())
+    pen.run(steps=20, verbose=True)
+
+
