0923d9555812c7689e3a529d9403fa44e4dfad97
ruperty <rupert@moonsit.co.uk>
2023-03-13 20:42:52 +0000

    u

--- Libraries/python/pctlocal/tests/ga/pctobject/testing/test-tcp-client.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/testing/test-tcp-client.py
@@ -1,10 +1,12 @@
 
 
 from pct.environments import WebotsWrestler
+from pct.functions import Constant
 
 wrestler = WebotsWrestler()
+wrestler.add_link(Constant(1, name='mycon'))
 
-loops=1000
+loops=2000
 for i in range(loops):
     if i==loops-1:
         wrestler.done=True
