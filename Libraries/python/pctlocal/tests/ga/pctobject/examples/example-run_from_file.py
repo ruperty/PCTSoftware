a3ba7518faa219fd75802a13697f1cb2137b4e87
ruperty <rupert@moonsit.co.uk>
2023-04-21 16:49:55 +0100

    u

--- Libraries/python/pctlocal/tests/ga/pctobject/examples/example-run_from_file.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/examples/example-run_from_file.py
@@ -66,7 +66,9 @@ data = [
     [4, 'WebotsWrestler', 'ga-000.870-s001-3x6-m001-e8993f3235b484cd5a869600d6d5a374', 'WW01-RewardError-CurrentError-Mode01', {}, [], True],
     [5, 'WebotsWrestler', '', 'WW01-03-RewardError-CurrentError-Mode01', {}, [], False],
     [6, 'WebotsWrestler', 'ga-001.012-s001-8x7-m001-c589fa85b67d512975680f7265d52149', 'WW01-04-RewardError-CurrentError-Mode01', {}, [], False],
-    [7, 'WebotsWrestler', 'ga-000.023-s001-5x6-m001-a0025b57841f3289e33101bcd2c7c21f', 'WW01-01-RewardError-CurrentError-Mode01', {}, [], False]
+    [7, 'WebotsWrestler', 'ga-000.023-s001-5x6-m001-a0025b57841f3289e33101bcd2c7c21f', 'WW01-01-RewardError-CurrentError-Mode01', {}, [], False],
+    [8, 'WebotsWrestler', 'ga-000.554-s001-3x5-m001-396442a24782fbf5d945531d302e886c', 'WW01-05-RewardError-CurrentError-Mode01', {}, [], False]
+
 
     ]
 
@@ -77,7 +79,7 @@ if test == 100:
       
 if test == 10:
     #runit(data[4]) # good one
-    runit(data[4])
+    runit(data[8])
 
 
 
