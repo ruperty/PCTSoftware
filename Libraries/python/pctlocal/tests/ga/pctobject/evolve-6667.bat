56b0a83469b9b7308f5f8308f0305194e3894a88
ruperty <rupert@moonsit.co.uk>
2023-04-24 21:50:09 +0100

    u

--- Libraries/python/pctlocal/tests/ga/pctobject/evolve-6667.bat
+++ Libraries/python/pctlocal/tests/ga/pctobject/evolve-6667.bat
@@ -1,4 +1,4 @@
-python examples\evolve.py WebotsWrestler WW01-06-RewardError-CurrentError-Mode01 -p 6666
+python examples\evolve.py WebotsWrestler WW01-06-RewardError-CurrentError-Mode01 -p 6667
 
 
 
