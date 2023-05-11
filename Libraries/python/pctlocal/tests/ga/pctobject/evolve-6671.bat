56281f0d4b33152892bfc6bcd150bc1bd42d11de
ruperty <rupert@moonsit.co.uk>
2023-05-11 14:50:08 +0100

    upd

--- Libraries/python/pctlocal/tests/ga/pctobject/evolve-6671.bat
+++ Libraries/python/pctlocal/tests/ga/pctobject/evolve-6671.bat
@@ -4,8 +4,8 @@ set PORT=6671
 set PYTHONPATH=%PYTHONPATH%;%USERPROFILE%\Versioning\PCTSoftware\Libraries\python\webots\controllers;C:\Program Files\Webots\lib\controller\python;%USERPROFILE%\Versioning\PCTSoftware\Libraries\python\pctlocal\tests\ga\pctobject
 
 
-set CONFIG=WW01-11-RewardError-CurrentError-Mode04
-%USERPROFILE%\AppData\Local\Programs\Python\Python39\python.exe %USERPROFILE%\Versioning\PCTSoftware\Libraries\python\pctlocal\tests\ga\pctobject\examples\evolve.py %ENV% %CONFIG% -p %PORT%
+::set CONFIG=WW01-11-RewardError-CurrentError-Mode04
+::%USERPROFILE%\AppData\Local\Programs\Python\Python39\python.exe %USERPROFILE%\Versioning\PCTSoftware\Libraries\python\pctlocal\tests\ga\pctobject\examples\evolve.py %ENV% %CONFIG% -p %PORT%
 
 
 set CONFIG=WW01-12-RewardError-CurrentError-Mode04
