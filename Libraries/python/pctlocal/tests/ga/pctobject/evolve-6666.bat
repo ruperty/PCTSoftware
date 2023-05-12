694796489d479d16750ca36835a8084066ab6206
ruperty <rupert@moonsit.co.uk>
2023-05-12 14:22:23 +0100

    upd

--- Libraries/python/pctlocal/tests/ga/pctobject/evolve-6666.bat
+++ Libraries/python/pctlocal/tests/ga/pctobject/evolve-6666.bat
@@ -4,9 +4,13 @@ set ENV=WebotsWrestler
 set PORT=6666
 
 
+set CONFIG=WW01-01-RewardError-CurrentError-Mode01
 
-set CONFIG=WW01-10-RewardError-CurrentError-Mode03
-%USERPROFILE%\AppData\Local\Programs\Python\Python39\python.exe %USERPROFILE%\Versioning\PCTSoftware\Libraries\python\pctlocal\tests\ga\pctobject\examples\evolve.py %ENV% %CONFIG% -p %PORT% > out.txt
+%USERPROFILE%\AppData\Local\Programs\Python\Python39\python.exe %USERPROFILE%\Versioning\PCTSoftware\Libraries\python\pctlocal\tests\ga\pctobject\examples\evolve.py %ENV% %CONFIG% -p %PORT%
+
+set CONFIG=WW01-02-RewardError-CurrentError-Mode01
+
+%USERPROFILE%\AppData\Local\Programs\Python\Python39\python.exe %USERPROFILE%\Versioning\PCTSoftware\Libraries\python\pctlocal\tests\ga\pctobject\examples\evolve.py %ENV% %CONFIG% -p %PORT%
 
 
 
