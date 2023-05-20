d28ec3366daf78106d1d191cdfe69bf534b3bbc4
ruperty <rupert@moonsit.co.uk>
2023-05-20 10:24:17 +0100

    u

--- Libraries/python/pctlocal/tests/ga/pctobject/evolve-YOUNG2.bat
+++ Libraries/python/pctlocal/tests/ga/pctobject/evolve-YOUNG2.bat
@@ -6,11 +6,11 @@ set PYTHONPATH=%PYTHONPATH%;%USERPROFILE%\Versioning\PCTSoftware\Libraries\pytho
 
 
 
-set CONFIG=WW01-07-RewardError-CurrentError-Mode02 
-%USERPROFILE%\AppData\Local\Programs\Python\Python39\python.exe %USERPROFILE%\Versioning\PCTSoftware\Libraries\python\pctlocal\tests\ga\pctobject\examples\evolve.py %ENV% %CONFIG% -p %PORT%
+::set CONFIG=WW01-07-RewardError-CurrentError-Mode02 
+::%USERPROFILE%\AppData\Local\Programs\Python\Python39\python.exe %USERPROFILE%\Versioning\PCTSoftware\Libraries\python\pctlocal\tests\ga\pctobject\examples\evolve.py %ENV% %CONFIG% -p %PORT%
 
 set CONFIG=WW01-08-RewardError-CurrentError-Mode02 
-%USERPROFILE%\AppData\Local\Programs\Python\Python39\python.exe %USERPROFILE%\Versioning\PCTSoftware\Libraries\python\pctlocal\tests\ga\pctobject\examples\evolve.py %ENV% %CONFIG% -p %PORT%
+%USERPROFILE%\AppData\Local\Programs\Python\Python39\python.exe %USERPROFILE%\Versioning\PCTSoftware\Libraries\python\pctlocal\tests\ga\pctobject\examples\evolve.py -p %PORT% %ENV% %CONFIG% 
 
 
 pause
