11a0539288678974c147a6c210f833a80a7e3fc3
ruperty <rupert@moonsit.co.uk>
2023-06-11 20:46:51 +0100

    updated environments manually

--- Libraries/python/pctlocal/tests/ga/pctobject/examples/evolve.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/examples/evolve.py
@@ -30,10 +30,18 @@ if __name__ == '__main__':
 
         out_dir= get_gdrive() + f'data{sep}ga{sep}'
 
-        creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-        creator.create("Individual", HPCTIndividual, fitness=creator.FitnessMax)
+        max = False
+        if max:
+                creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+                creator.create("Individual", HPCTIndividual, fitness=creator.FitnessMax)
+                flip=True
+                min=False
+        else:
+                creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+                creator.create("Individual", HPCTIndividual, fitness=creator.FitnessMin)
+                flip=False
+                min=True
 
-        min=False
 
         toolbox = base.Toolbox()
         CommonToolbox.getInstance().set_toolbox(toolbox)
@@ -76,7 +84,7 @@ if __name__ == '__main__':
         #         print(f'Sleeping for {10-i} seconds')
         #         sleep(1)
 
-        hash_num, desc = hep.configure_evolver_from_properties_file(file=file, print_properties=True, verbose=verbose, toolbox=toolbox,  min=min)
+        hash_num, desc, properties_str = hep.configure_evolver_from_properties_file(file=file, print_properties=True, verbose=verbose, toolbox=toolbox,  min=min)
 
         # logging info
         now = datetime.now() # current date and time
@@ -87,26 +95,21 @@ if __name__ == '__main__':
         logging.basicConfig(filename=log_file, level=logging.INFO,    format="%(asctime)s.%(msecs)03d:%(levelname)s:%(module)s.%(lineno)d %(message)s",datefmt= '%H:%M:%S'    )
         logger = logging.getLogger(__name__)
         logger.info("Evolving {} ".format(env_name))
+        logger.info(properties_str)
 
-        try:
-                hep.run_configured_evolver( file=file, print_properties=True, draw_file=True, out_dir=out_dir, hash_num=hash_num,
+        # try:
+        hep.run_configured_evolver( file=file, print_properties=True, draw_file=True, out_dir=out_dir, hash_num=hash_num,
                                 output=output, overwrite=overwrite, node_size=node_size, font_size=font_size, log=True)
 
-        except Exception as e:
-                if hasattr(e, 'message'):
-                        print(e.message)
-                        logger.info(e.message)
-                else:
-                        print(e)
-                        logger.info(e)
+        # except Exception as e:
+        #         if hasattr(e, 'message'):
+        #                 print(e.message)
+        #                 logger.info(e.message)
+        #         else:
+        #                 print(e)
+        #                 logger.info(e)
 
 
-
-
-        # logger.info('Start evolve_ww')
-        # hep.configure_evolver_from_properties_file(file=file, print_properties=True, verbose=verbose, toolbox=toolbox, draw_file=draw_file, 
-        #                                         out_dir=out_dir, local_out_dir=local_out_dir, output=output, overwrite=overwrite, 
-        #                                         node_size=node_size, font_size=font_size, min=min)
         
 
 
