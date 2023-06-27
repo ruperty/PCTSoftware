52c66a9667ead78b587dcf70fec33a669b86060d
ruperty <rupert@moonsit.co.uk>
2023-06-27 20:56:19 +0100

    work on mc

--- Libraries/python/pctlocal/tests/ga/pctobject/impl/evolve-multi.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/impl/evolve-multi.py
@@ -1,7 +1,90 @@
 
 
 
+import time
 import argparse
+from os import sep
+from cutils.paths import get_gdrive
+from eepct.hpct import HPCTEvolveProperties
+
+def evolve(args):
+    seed=args['seed']
+    file=args['file']
+    verbosed=args['verbosed']
+    gens=args['gens']
+    pop=args['pop']
+    verbose=args['verbose']
+    overwrite=args['overwrite']
+    #{'seed': i, 'file': args.file, 'verbosed':args.verbosed, 'gens':args.gens, 'pop':args.pop, 
+    #           'test':args.test, 'overwrite':args.overwrite, 'move':move, 'verbose': args.verbose}
+    
+    print(f'Start seed {seed}')
+    tic = time.perf_counter()
+    
+
+    out_dir= get_gdrive() + f'data{sep}ga{sep}'
+
+
+
+            max = False
+        if max:
+                creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+                creator.create("Individual", HPCTIndividual, fitness=creator.FitnessMax)
+                flip=True
+                min=False
+        else:
+                creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+                creator.create("Individual", HPCTIndividual, fitness=creator.FitnessMin)
+                flip=False
+                min=True
+
+
+        toolbox = base.Toolbox()
+        CommonToolbox.getInstance().set_toolbox(toolbox)
+
+        node_size, font_size=150, 10
+
+        root = get_root_path()
+
+
+
+        file = root + 'Versioning/PCTSoftware/Libraries/python/pctlocal/tests/ga/pctobject/configs/' + env_name +'/'+ filename + ".properties"
+
+        local_out_dir = 'output/'  + filename 
+        draw_file= local_out_dir + '/' + filename + '-evolve-best' + '.png'
+
+        debug= 0 #0 #3 # details of population in each gen, inc. mutate and merge
+        hpct_verbose= False #True # log of every control system iteration
+        evolve_verbose =  1 #2 # output of evolve iterations, 2 for best of each gen
+
+       
+
+        verbose={ 'debug': debug, 'evolve_verbose': evolve_verbose, 'display_env': args.display_env, 'hpct_verbose':hpct_verbose, 
+                'save_arch_gen': args.save_arch_gen, 'run_gen_best':args.run_gen_best}
+        
+
+
+    hep = HPCTEvolveProperties()
+    output=True
+    overwrite=True
+
+    hash_num, desc, properties_str = hep.configure_evolver_from_properties_file(file=file, seed=seed, print_properties=True, verbose=verbose, toolbox=toolbox,  min=min)
+
+    hep.run_configured_evolver( file=file, print_properties=True, draw_file=True, out_dir=out_dir, hash_num=hash_num,
+                                        output=output, overwrite=overwrite, node_size=node_size, font_size=font_size, log=True)
+
+
+    out,evr,score=evolve_from_properties_file(out_dir=out_dir,file=file, verbose=verbosed, seed=seed, gens=gens, pop_size=pop, 
+          output=True, draw=False, print_properties=verbose, overwrite=overwrite, log=True)
+    
+
+
+    if out != None:
+        toc = time.perf_counter()
+        elapsed = toc-tic        
+        print(f'Seed {seed} Evolve time: {elapsed:4.2f}')
+
+
 
 if __name__ == '__main__':
     
--- Libraries/python/pctlocal/tests/ga/pctobject/impl/run_from_file.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/impl/run_from_file.py
@@ -6,7 +6,11 @@ from eepct.hpct import HPCTIndividual, HPCTEvolveProperties
 
 # py -m impl.run_from_file -f "G:\My Drive\data\ga\MountainCarContinuousV0\MC08-ReferencedInputsError-RootMeanSquareError-Mode04\ga-000.548-s013-2x2-m004-5a08e6cdc09769db0267a14f0634b051.properties"
 # py -m impl.run_from_file -f "G:\My Drive\data\ga\MountainCarContinuousV0\MC06-ReferencedInputsError-RootMeanSquareError-Mode03\ga-000.554-s068-2x2-m003-5342c97128d9ad23a0fea14a6d9c05e5.properties"
-    
+# py -m impl.run_from_file -f "G:\My Drive\data\ga\MountainCarContinuousV0\MC04-ReferencedInputsError-RootMeanSquareError-Mode02\ga-000.632-s072-2x2-m002-89f5392f56c60cdc79cf108aa20461ce.properties"
+# python -m impl.run_from_file -f "G:\My Drive\data\ga\testfiles\ga-000.334-s068-2x2-6655-8193255f62b3d17188329b5537b3631d-pctobject.properties"
+
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
