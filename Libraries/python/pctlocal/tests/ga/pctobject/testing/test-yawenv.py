11606cfa6078d2d5d6c62fa89adae2b5749877e3
ruperty <rupert@moonsit.co.uk>
2023-08-02 17:26:28 +0100

    u

--- Libraries/python/pctlocal/tests/ga/pctobject/testing/test-yawenv.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/testing/test-yawenv.py
@@ -12,13 +12,17 @@ test = 1
 if test ==1:
     # seed 3 circles anti-clockwise
     yaw = WindTurbine() 
-    yaw.add_link(Constant(0))
+    con = Constant(1, namespace=yaw.namespace)
+    yaw.add_link(con)
     env_props={'series': 'steady'}
     yaw.set_properties(env_props)
     print(yaw.get_config())
-
-    for i in range(100):
+    actions = [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1,1,1,1,1,1,1,1, 1, 1]
+    for i in range(len(actions)):
+        con.set_value(actions[i])
+        print(i, end=" ")
         yaw.run(steps=steps, verbose=True)
+        print()
     yaw.close()
 
     # git test
