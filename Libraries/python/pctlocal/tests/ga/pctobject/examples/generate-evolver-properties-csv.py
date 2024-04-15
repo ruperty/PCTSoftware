d45769b00ccf3595c606ff4d948e40906985f0ef
ruperty <rupert@moonsit.co.uk>
2024-04-15 20:38:10 +0100

    up

--- Libraries/python/pctlocal/tests/ga/pctobject/examples/generate-evolver-properties-csv.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/examples/generate-evolver-properties-csv.py
@@ -48,7 +48,7 @@ if test == 'WindTurbine':
     if test == "steady":
         filename = 'wt' + sep +'configs-wt-0001-0616-steady.csv'
         args = "-b -pl scEdges -p evolve1 -rp \"{'comparisons' : True, 'comparisons_print_plots': True}\" -c 6 "
-        args = "-b -pl scEdges -p evolve1-batch -rp \"{'comparisons' : True, 'comparisons_print_plots': True}\" -c 6 -s 2 -i 4"
+        # args = "-b -pl scEdges -p evolve1-batch -rp \"{'comparisons' : True, 'comparisons_print_plots': True}\" -c 6 -s 2 -i 4"
         # args = "-b -o -pl scEdges -p test-evolve -rp \"{'comparisons' : True, 'comparisons_print_plots': True}\" -c 3 -s 1 -i 3"
         # args = "-b -l -o -pl scEdges -p evolve" 
 
