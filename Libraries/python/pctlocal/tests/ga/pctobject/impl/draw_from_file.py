3d9cc76e199b168c5754f80808e54b4e3d7153cd
ruperty <rupert@moonsit.co.uk>
2024-04-28 21:08:26 +0100

    up

--- Libraries/python/pctlocal/tests/ga/pctobject/impl/draw_from_file.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/impl/draw_from_file.py
@@ -23,9 +23,16 @@ def drawit(filename=None, outdir=None, move=None, funcdata=False, font_size=6, n
     if suffixes:
         hpct.set_suffixes()
     print(hpct.formatted_config(3))
-    hpct.summary()
+    # hpct.summary()
     hpct.draw(file=draw_file, move=move, with_edge_labels=True, font_size=font_size, node_size=node_size, funcdata=funcdata)
     print('Image saved to '+draw_file)
+
+    hpct.remove_links()
+    draw_file = outdir + sep + 'draw-'+filenamePrefix + '_A' +'.png'
+    hpct.draw(file=draw_file, move=move, with_edge_labels=True, font_size=font_size, node_size=node_size, funcdata=funcdata)
+    print(hpct.formatted_config(3))
+    print('Image saved to '+draw_file)
+
 
 
 # python -m impl.draw_from_file -f "G:\My Drive\data\ga\MountainCarContinuousV0\MC00-ReferencedInputsError-RootMeanSquareError-Mode00\ga-000.385-s064-2x2-m000-f46606db9aa4aabc0af650882cabb6ac.properties" -o "G:\My Drive\data\ga\MountainCarContinuousV0\MC00-ReferencedInputsError-RootMeanSquareError-Mode00\f46606db9aa4aabc0af650882cabb6ac" -m "{'IV':[0.15,0.2],'IP':[-0.8,0.1], 'OL0C0p':[-0.4,-0.2],'OL0C1p':[0,-0.2], 'OL1C0p':[0,-0.1], 'MountainCarContinuousV0':[-.7,-0.5], 'Action1ws':[-0.4,-0.3]}"
@@ -54,5 +61,10 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    drawit(filename=args.file, outdir=args.outdir, funcdata=args.funcdata, font_size=args.font_size, node_size=args.node_size, move=eval(args.move), suffixes=args.suffixes)
+    if args.move is None:
+        move = {}
+    else:
+        move = eval(args.move)
+
+    drawit(filename=args.file, outdir=args.outdir, funcdata=args.funcdata, font_size=args.font_size, node_size=args.node_size, move=move, suffixes=args.suffixes)
 
