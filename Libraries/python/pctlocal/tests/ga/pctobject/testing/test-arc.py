d7cbbae12a0a4cf678b8944033b8d14db659f33b
ruperty <rupert@moonsit.co.uk>
2024-06-27 19:44:08 +0100

    up

--- Libraries/python/pctlocal/tests/ga/pctobject/testing/test-arc.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/testing/test-arc.py
@@ -6,16 +6,22 @@ test = "mg+days"
 
 if test == "mg+days":
     env = ARC()
-    
-    env.add_link(Constant(1))
-    env.add_link(Constant(2))
-    properties = {'dir': 'C:\\packages\\arc-prize-2024\\training', 'code':'1_007bbfb7.dat'}
+    env.add_link(Constant(3))
+    env.add_link(Constant(5))
+    properties = {'dir': 'C:\\packages\\arc-prize-2024\\training', 'code':'1_007bbfb7.dat', 'fitness_type': 'dim_only'}
     env.set_properties(properties)
+    env.set_render(True)
     env.reset()
-    state = env()
-    env.summary()
+
+    for i in range(5):
+        state = env()
+        env.summary()    
+        print()
     print(env.get_config())
+    print()
     print(env.output_string())    
+    print()
     print(state)
 
+    env.close()
 
