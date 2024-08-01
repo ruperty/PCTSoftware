b0cf4865fa41f33c2b9ba9d4b6bc86bf2f92cbb0
ruperty <rupert@moonsit.co.uk>
2024-08-01 21:43:15 +0100

    u

--- Libraries/python/pctlocal/tests/ga/pctobject/examples/generate-arc.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/examples/generate-arc.py
@@ -12,42 +12,53 @@ from os import sep
 
 from eepct.hpct import HPCTGenerateEvolvers
 
+def process(filename,common_configs, args, cmd, initial_index, batch):
+    file = 'configs'+ sep + filename
+
+    hge = HPCTGenerateEvolvers(common_configs=common_configs)  
+    hge.process_csv(file, args, cmdline=cmd, initial_index=initial_index, batch=batch)
+
+
 # test = 'simple'
-test = 'dims_only'
+project = 'dims_only'
 
 args = "-i 1 -s 93"
 cmd='impl.evolve'
 # initial_index=1
 batch = 10
+num_evals = 1
+pop_size =  100
+gens = 25
+evolve_termination_value = 0
+runs = 250
 
-if test == 'dims_only':
+if project == 'dims_only':
 
-    # set = 'env_only'
-    set = 'inputs'
     filename = 'ar' + sep +'configs-dims-only.csv'
-    args = "-b -pl scEdges,scZero -p dims-only -o"
-    num_evals = 1
-    pop_size =  100
-    gens = 25
-    evolve_termination_value = 0
+    args = f'-b -pl scEdges,scZero -p {project} -o'
+    initial_index=1
+    qty = 7
 
-    if set == 'env_only':
-        initial_index=1
-        properties = { 'code':'007bbfb7',  'dataset': 'train', 'control_set': ['dims'], 'input_set': ['env']}
-    elif set == 'inputs':
-        initial_index=8
-        properties = { 'code':'007bbfb7',  'dataset': 'train', 'control_set': ['dims'], 'input_set': ['env', 'inputs']}
+    ls = [ ['env_only'] , ['env', 'inputs'] ]
+    for input_set in ls:
+        for index in range(2):
+            if index == 0:
+                properties = { 'code':'007bbfb7',  'dataset': 'train', 'control_set': ['dims'], 'input_set': input_set, 'index' : 0}
+            else:
+                properties = { 'code':'007bbfb7',  'dataset': 'train', 'control_set': ['dims'], 'input_set': input_set}
 
-    common_configs = {'env' : 'ARC', 'seed': 1, 'arch_name' : 'ARC', 'pop_size' : pop_size, 'gens': gens, 'evolve_termination_value': evolve_termination_value,
-                    'attr_mut_pb' : 1, 'structurepb' : 0.9, 'runs' : 100, 'lower_float' : -1, 'upper_float' : 1, 'min_levels_limit': 1, 
+            common_configs = {'env' : 'ARC', 'seed': 1, 'arch_name' : 'ARC', 'pop_size' : pop_size, 'gens': gens, 'evolve_termination_value': evolve_termination_value,
+                    'attr_mut_pb' : 1, 'structurepb' : 0.9, 'runs' : runs, 'lower_float' : -1, 'upper_float' : 1, 'min_levels_limit': 1, 
                     'max_levels_limit': 2, 'min_columns_limit': 1, 'max_columns_limit': 2, 'early_termination': True, 'p_crossover': 0.9, 
                     'p_mutation': 0.9, 'num_evals': num_evals, 'error_limit': 10000, 'environment_properties': properties}
 
-if test == 'simple':
+            process(filename,common_configs, args, cmd, initial_index, batch)
+            initial_index += qty
+
+if project == 'simple':
 
     from socket import gethostname
     
-
     user = 'ruper' if gethostname() == 'DESKTOP-5O07H5P' else 'ryoung'
     initial_index=20
     filename = 'ar' + sep +'configs-simple.csv'
@@ -62,9 +73,5 @@ if test == 'simple':
                     'max_levels_limit': 4, 'min_columns_limit': 1, 'max_columns_limit': 4, 'early_termination': True, 'p_crossover': 0.9, 
                     'p_mutation': 0.9, 'num_evals': num_evals, 'error_limit': 10000, 'environment_properties': properties}
 
+    process(filename,common_configs, args, cmd, initial_index, batch)
 
-
-file = 'configs'+ sep + filename
-
-hge = HPCTGenerateEvolvers(common_configs=common_configs)  
-hge.process_csv(file, args, cmdline=cmd, initial_index=initial_index, batch=batch)
