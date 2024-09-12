3f30c42c2f499355d3e260fa5c1e6fbaebb58fd9
ruperty <rupert@moonsit.co.uk>
2024-09-12 21:21:11 +0100

    Update generate-arc.py and cmds-train.txt

--- Libraries/python/pctlocal/tests/ga/pctobject/examples/generate-arc.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/examples/generate-arc.py
@@ -37,6 +37,10 @@ python examples/generate-arc.py -f configs-simple-00000003.csv -c 00000003 -p si
 python examples/generate-arc.py -f configs-simple-00000004.csv -c 00000004 -p simple-00000004 -ii 151 -a "-i 3" -ep "{ 'dir': '/tmp/arc-prize-2024', 'file_prefix':'arc-agi_simple_', 'code':'00000004',  'dataset': 'train', 'control_set': ['cells'], 'input_set': ['env', 'inputs'], 'tolerances': {'atARCresolved': 0.5, 'atARCdisplay': 0.5}, 'fitness': 'AverageMaxOfDiff'}" >> configs/ar/cmds-simple.txt
 
 
+python examples/generate-arc.py -f configs-train-5582e5ca.csv -p train-5582e5ca -ii 201 -a "-i 3" -ep "{ 'dir': '/tmp/arc-prize-2024', 'file_prefix':'arc-agi_training_', 'code':'5582e5ca',  'dataset': 'train', 'control_set': ['cells'], 'input_set': ['env', 'inputs'], 'tolerances': {'atARCresolved': 0.5, 'atARCdisplay': 0.5}, 'fitness': 'AverageMaxOfDiff'}" > configs/ar/cmds-train.txt
+
+
+
 python examples/generate-arc.py -f configs-simple-00000003.csv -c 00000003 -p simple-00000003 -ii 121 -a "-i 3" -pop 1000 -o -ep "{ 'dir': '/tmp/arc-prize-2024', 'file_prefix':'arc-agi_simple_', 'code':'00000003',  'dataset': 'train', 'control_set': ['cells'], 'input_set': ['env', 'inputs'], 'tolerances': {'atARCresolved': 0.5, 'atARCdisplay': 0.5}, 'fitness': 'AverageMaxOfDiff'}" >> configs/ar/cmds-simple.txt
 
 
