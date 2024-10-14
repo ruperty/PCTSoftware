9a5529b558d0cfde0d006418f0601b8c92c3c001
ruperty <rupert@moonsit.co.uk>
2024-10-14 20:23:36 +0100

    add 006 configurations

--- Libraries/python/windturbine-rl/gen_cmds.py
+++ Libraries/python/windturbine-rl/gen_cmds.py
@@ -1,5 +1,11 @@
 
 
 
-for i in range(0,100, 1):
-    print(f'python steady_script.py -s {1+i*10} -i 10 > steadyA{i:03d}.log')
+# for i in range(0,100, 1):
+#     print(f'python steady_script.py -s {1+i*10} -i 10 > steadyA{i:03d}.log')
+
+
+for i in range(375,1000, 25):
+    print(f'python run_script.py -d steady_wind.csv -m C:/Users/ruper/Versioning/PCTSoftware/Libraries/python/windturbine-rl/results/steady/1104-028-20241009-214610/steady_wind.zip -s {i} -i 25')
+
+
