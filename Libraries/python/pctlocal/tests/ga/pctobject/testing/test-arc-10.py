e0cdc37cbfc887795e9af329893e509933e18a86
ruperty <rupert@moonsit.co.uk>
2024-10-24 21:05:19 +0100

    up

--- Libraries/python/pctlocal/tests/ga/pctobject/testing/test-arc-10.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/testing/test-arc-10.py
@@ -7,19 +7,27 @@ Created on Thu Feb  4 19:08:59 2021
 """
 
 
-
+from os import sep
 
 from pct.putils import Timer, PCTRunProperties
 from pct.hierarchy import PCTHierarchy
 from pct.errors import BaseErrorCollector
+import os
 
-plots = []
+# plots = "scEdges,scFitness"
+plots = "scEdges"
 render=True
-runs=10
+runs=250
 early_termination = True
 hpct_verbose = False
-history = False
+history = True
 suffixes=False
+plots_dir = "c:/tmp/ARC"
+if not os.path.exists(plots_dir):
+    os.makedirs(plots_dir)
+plots_figsize=(15,4)
+run = True #False
+
 # file = "G:\\My Drive\\data\\ga\\MicroGrid\\RewardError-RootMeanSquareError-Mode04\\ga-000.017-s001-3x5-m004-MG0001-9b7851aa082d1178ee05750f4b5815ce.properties"
 
 file = "G:\My Drive\data\ga\ARC\FitnessError-MovingAverageError-Mode19\ga-062.617-s003-1x1-m019-ARC0375-7b4718a3de3ff677ac24d839feaf0507-consolidated.properties"
@@ -56,20 +64,45 @@ if hpct_verbose:
     hierarchy.summary()
     print(hierarchy.formatted_config())
 
-hierarchy.get_node(0,0).get_function('reference').set_value(3)
+hierarchy.get_node(0,0).get_function('reference').set_value(1)
 print(hierarchy.get_node(0,0).get_function('reference').get_value())
 
 percs = hierarchy.get_node(0,0).get_function('perception')
 # print(percs)
 counter = 1
 for link in percs.get_links():
-    print(f"{counter}: {link.get_name()} - {link.get_value()} {percs.weights[counter-1]}")
+    percs.weights[counter-1] = 0
+    if link.get_name() == 'IE001' :
+        percs.weights[counter-1] = 1
+    if link.get_name() == 'II002':
+        percs.weights[counter-1] = -1
+    # print(f"{counter}: {link.get_name()} - {percs.weights[counter-1]}")
     counter += 1
+
+output = hierarchy.get_node(0,0).get_function('output')
+output.smooth_factor = 0.147
+print(output.get_config())
+
+# actions = hierarchy.get_postprocessor()
+# for action in actions:
+#     print(action.get_config())
 
 # hierarchy.summary()
 
-# hierarchy.run(runs, hpct_verbose)
+if run:
+    hierarchy.run(runs, hpct_verbose)
 
+    if history:
+        if plots:
+            plots = hierarchy.get_plots_config(plots, "xx")
+            
+            for plot in plots:
+                plotfile=None
+                if plots_dir:
+                    plotfile = plots_dir + sep + plot['title'] + '-' + str(hierarchy.get_namespace()) + '.png'
+                fig = hierarchy.hierarchy_plots(title=plot['title'], plot_items=plot['plot_items'], figsize=plots_figsize, file=plotfile)
+                import matplotlib.pyplot as plt
+                plt.close(fig)  # Close the figure here
 
 env.close()
 # print(f'Score={score:0.3f}')
