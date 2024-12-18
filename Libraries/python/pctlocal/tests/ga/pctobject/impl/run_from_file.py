676058fa96c20ece9d80285f2ef45a9e46973194
ruperty <rupert@moonsit.co.uk>
2024-12-18 17:09:24 +0000

    refactor: update launch configuration and add new test file paths

--- Libraries/python/pctlocal/tests/ga/pctobject/impl/run_from_file.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/impl/run_from_file.py
@@ -32,6 +32,11 @@ from pct.environment_processing import EnvironmentProcessingFactory
 # python  -m impl.run_from_file -e -d -o c:/tmp/arc -f "G:\My Drive\data\ga\ARC\FitnessError-MovingAverageError-Mode02\ga-063.080-s004-1x1-m002-ARC0154-2e60e0477d1bd7f569bad1b748111bf1.properties" -eep "{'dir': '/tmp/arc-prize-2024', 'file_prefix': 'arc-agi_simple_', 'code': '00000004', 'dataset': 'test', 'control_set': ['cells'], 'input_set': ['env', 'inputs'], 'runs': 750, 'history': 10, 'initial': 100}"
 
 """
+
+python -m impl.run_from_file -f "g:\My Drive\data\ga\MountainCarContinuousV0\MC08-ReferencedInputsError-RootMeanSquareError-Mode04\ga-000.331-s032-2x2-m004-cdf7cc1497ad143c0b04a3d9e72ab783.properties"
+
+python -m impl.run_from_file -f "G:\My Drive\data\ga\WindTurbine\RewardError-SummedError-Mode05\ga--1362.401-s003-4x3-m005-WT0538-bddf277b0f729cc630efacf91b9f494f.properties"
+
 python  -m impl.run_from_file -e -d -o c:/tmp/arc -f "G:\My Drive\data\ga\ARC\FitnessError-MovingAverageError-Mode19\ga-000.500-s001-1x1-m019-ARC0328-9799d017b565995c04c518b7e15c0e1c-consolidated.properties"
        
 """
