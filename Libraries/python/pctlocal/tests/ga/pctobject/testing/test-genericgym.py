274317d8fcc5b084f1fa73fe7e448d57a0b43eea
ruperty <rupert@moonsit.co.uk>
2025-02-10 20:47:32 +0000

    refactor: rename GymEnvInfo to GymMetaData and enhance action space handling

--- Libraries/python/pctlocal/tests/ga/pctobject/testing/test-genericgym.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/testing/test-genericgym.py
@@ -10,24 +10,23 @@ test = 0
 # create a method to extract the number of states possible
 # create a method to take a set of values and map them to the action space
 
-class GymEnvInfo:
+class GymMetaData:
     def __init__(self, env):
         self.action_space =  env.action_space
         self.observation_space = env.observation_space
         self.num_actions = self.get_num_actions()
+        self.action_array = True
+        if isinstance(self.action_space, gym.spaces.discrete.Discrete):
+            self.action_array = False
 
     def get_env_inputs_names(self):
         ninputs = self.observation_space.shape[0]
-
         env_inputs_names = [f"I{i}" for i in range(ninputs)]
-
         return env_inputs_names
 
     def get_env_inputs_indexes(self):
         ninputs = self.observation_space.shape[0]
-
         env_inputs_indexes = [i for i in range(ninputs)]
-
         return env_inputs_indexes
 
     def get_num_actions(self):
@@ -37,6 +36,9 @@ class GymEnvInfo:
             return self.action_space.shape[0]
         else:
             return None
+
+    def is_action_array(self):
+        return self.action_array
 
     def get_action_limits(self):
         return self.action_space.low, self.action_space.high
@@ -50,8 +52,6 @@ class GymEnvInfo:
                 return np.array([0], dtype=self.action_space.dtype), np.array([1], dtype=self.action_space.dtype)
             elif self.action_space.n == 3:
                 return np.array([-1], dtype=self.action_space.dtype), np.array([0], dtype=self.action_space.dtype), np.array([1], dtype=self.action_space.dtype)
-        
-    
 
     def get_num_states(self):
         if hasattr(self.observation_space, 'shape'):
@@ -59,15 +59,16 @@ class GymEnvInfo:
         else:
             return None
 
-    def map_values_to_action_space(self, values):
-        if isinstance(self.action_space, gym.spaces.box.Box):
-            vals = np.clip(values, self.action_space.low, self.action_space.high)
+    @classmethod
+    def map_values_to_action_space(cls, action_space, values):
+        if isinstance(action_space, gym.spaces.box.Box):
+            vals = np.clip(values, action_space.low, action_space.high)
             return vals
-        if isinstance(self.action_space, gym.spaces.discrete.Discrete):
-            con = self.action_space.contains(values)
-            if self.action_space.n == 2:
+        if isinstance(action_space, gym.spaces.discrete.Discrete):
+            con = action_space.contains(values)
+            if action_space.n == 2:
                 return np.where(values > 0.5, 1, 0)
-            elif self.action_space.n == 3:
+            elif action_space.n == 3:
                 return np.where(values > 0.5, 1, np.where(values < -0.5, -1, 0))
 
 if test == 0:
@@ -77,24 +78,19 @@ if test == 0:
     envs = [ 'Acrobot-v1', 'CartPole-v1', 'MountainCarContinuous-v0', 'Pendulum-v1']
     for env_name in envs:
         env = gym.make(env_name) 
-        env_info = GymEnvInfo(env)
+        env_info = GymMetaData(env)
         print('env_name', env_name)
         print("get_env_inputs_indexes", env_info.get_env_inputs_indexes())
         print("get_env_inputs_names", env_info.get_env_inputs_names())
         print("self.action_space", env_info.action_space)
         print("get_num_actions", env_info.get_num_actions())
         print("get_action_space_bounds", env_info.get_action_space_bounds())
-        print("map_values_to_action_space", env_info.map_values_to_action_space(0.51))
+        print("map_values_to_action_space", GymMetaData.map_values_to_action_space(env.action_space, 0.51))
+        if env_info.is_action_array():
+            print("map_values_to_action_space", GymMetaData.map_values_to_action_space(env.action_space, [-5.1, -.9,.9, 1.2]))
 
         
         print()
-
-        # print("Action space bounds:", env_info.get_action_space_bounds())
-        # print("Number of actions:", env_info.get_num_actions())
-        # print("Number of input states:", env_info.get_num_states())
-        # # print("Mapped values to action space:", env_info.map_values_to_action_space([0.5]))
-        # print("Mapped values to action space:", env_info.map_values_to_action_space([3]))
-
 
 
 if test ==1:
