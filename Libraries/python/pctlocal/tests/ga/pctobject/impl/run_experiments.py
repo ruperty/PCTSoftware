9ebab67cefb622bb364358118569758dfbd20400
ruperty <rupert@moonsit.co.uk>
2025-05-25 19:58:48 +0100

    feat: update launch configuration and refactor run_experiments to handle project filtering and output checks

--- Libraries/python/pctlocal/tests/ga/pctobject/impl/run_experiments.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/impl/run_experiments.py
@@ -2,6 +2,7 @@
 
 
 from pct.experiments import CometExperimentManager
+import os
 
 """
 
@@ -12,38 +13,45 @@ python -m impl.run_experiments
 
 # Initialize the manager
 workspace = 'lunarlandercontinuous-v2'
-	
-# project_name = 'refinputs-currentrms'
-# score_threshold=0.05
-project_name = 'reward-summed'
-# project_name = 'inputs-rms'
-# project_name = 'refinputs-smooth'
-
-max = False
-score_threshold=0.05
-
-if project_name == 'reward-summed':
-    score_threshold=100
-    max = True
-
-
-num_runs=100
-
 
 manager = CometExperimentManager(workspace=workspace)
 
-# Test get_all_artifacts_sorted
-artifact_results = manager.get_all_artifacts_indexed()
-# print("Artifacts sorted by source experiment key:", artifacts)
+projects = manager.get_workspace_projects()
+print(f"Projects in workspace '{workspace}':", projects)
 
-# Test get_experiments_by_metrics
-experiments = manager.get_experiments_by_metrics(project_name=project_name, score_threshold=score_threshold, reward_threshold=10.0, max=max)
-# print("Filtered experiments:", experiments)
+# project_name = 'refinputs-currentrms'
+# score_threshold=0.05
+# project_name = 'reward-summed'
+# project_name = 'inputs-rms'
+# project_name = 'refinputs-smooth'
 
+num_runs=100
 
-output_csv = f'/tmp/artifacts/experiment_results_{project_name}.csv'
-manager.run_experiments_and_record_results(experiments=experiments, project_name=project_name, artifact_results=artifact_results, num_runs=num_runs, output_csv=output_csv)
-print(f"Results saved to {output_csv}")
+for project_name in projects:
+    if project_name == 'general' or project_name == 'test':
+        continue
+    
+    max = False
+    score_threshold=0.05
+
+    if project_name == 'reward-summed' or project_name == 'opt-reward-summed':
+        score_threshold=100
+        max = True
+
+    # Test get_all_artifacts_sorted
+    artifact_results = manager.get_all_artifacts_indexed()
+    # print("Artifacts sorted by source experiment key:", artifacts)
+
+    # Test get_experiments_by_metrics
+    experiments = manager.get_experiments_by_metrics(project_name=project_name, score_threshold=score_threshold, reward_threshold=10.0, max=max)
+    # print("Filtered experiments:", experiments)
+
+    output_csv = f'/tmp/artifacts/experiment_results_{project_name}.csv'
+    if os.path.exists(output_csv):
+        print(f"{output_csv} already exists, skipping...")
+        continue
+    manager.run_experiments_and_record_results(experiments=experiments, project_name=project_name, artifact_results=artifact_results, num_runs=num_runs, output_csv=output_csv)
+    print(f"Results saved to {output_csv}")
 
 
 
