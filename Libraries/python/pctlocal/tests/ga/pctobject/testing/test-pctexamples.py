b6dbd4e183d4e30e74045637eb1deaa47a6bb048
ruperty <rupert@moonsit.co.uk>
2025-07-30 12:43:51 +0100

    feat: enhance separator detection and update test cases for improved functionality

--- Libraries/python/pctlocal/tests/ga/pctobject/testing/test-pctexamples.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/testing/test-pctexamples.py
@@ -19,7 +19,8 @@ if test == "draw":
         run_hierarchy=False,
         image_params={
             'file': '/tmp/mountaincar_image.png',
-            'move': {'IV':[0, 0.6],'IP':[-0.6, 0.3],  'OL0C0sm':[-0.28, -0.2],'OL0C1sm':[0.28, -0.2], 'OL1C0sm':[0,-0.1], 'MountainCarContinuousV0':[-.7,-0.5], 'Action1ws':[-0.4,-0.3]},
+            # 'move': {'IV':[0, 0.6],'IP':[-0.6, 0.3],  'OL0C0sm':[-0.28, -0.2],'OL0C1sm':[0.28, -0.2], 'OL1C0sm':[0,-0.1], 'MountainCarContinuousV0':[-.7,-0.5], 'Action1ws':[-0.4,-0.3]},
+            'move': {'IV':[0, 0.05],'IP':[-0.6, 0.3],  'OL0C0sm':[-0.28, -0.2],'OL0C1sm':[0.28, -0.2], 'OL1C0sm':[0,-0.1], 'MountainCarContinuousV0':[-.7,-0.5], 'Action1ws':[-0.4,-0.3]},
             'with_labels': True,
             'with_edge_labels': True,
             # 'funcdata': True,
@@ -28,10 +29,9 @@ if test == "draw":
         }
     )
 
-# with_labels=True, with_edge_labels=False,  font_size=12, font_weight='bold', font_color='black', 
-            # color_mapping={'PL':'aqua','OL':'limegreen','CL':'goldenrod', 'RL':'red', 'I':'silver', 'A':'yellow'},
-            # node_size=500, arrowsize=25, align='horizontal', file=None, figsize=(8,8), move={}, draw_fig=True,
-            # node_color=None, layout={'r':2,'c':1,'p':2, 'o':0}, funcdata=False, interactive_mode=False, experiment=None):
+#  
+
+
     
 
 if test == "run":
