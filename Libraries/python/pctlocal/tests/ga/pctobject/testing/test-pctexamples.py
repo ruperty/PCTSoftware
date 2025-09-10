2aeedd65f49cab908854a176ca54e768877767b4
perceptualrobots <rupert@perceptualrobots.com>
2025-09-10 20:03:26 +0100

    fix: update config file path for LunarLander environment and adjust test parameters

--- Libraries/python/pctlocal/tests/ga/pctobject/testing/test-pctexamples.py
+++ Libraries/python/pctlocal/tests/ga/pctobject/testing/test-pctexamples.py
@@ -1,49 +1,55 @@
 
-from pct.pctexamples import PCTExamples 
+from pct.pctexamples import PCTExamples  # type: ignore
 import getpass
 
 # Define the config file path
 user = getpass.getuser()
-config_file = f'C:/Users/{user}/Versioning/python/nbdev/pct/nbs/testfiles/MountainCar/MountainCar-cdf7cc.properties'
 
 
-# Create only video (no plots)
+env = "LunarLander"
+# env = "MountainCar"
 
-test = "draw"
-# test = "run"
+# test = "draw"
+test = "run"
 # test = "video"
+suffixes = True
+
+if env == "LunarLander":
+    config_file = f'C:/Users/{user}/Versioning/python/nbdev/pct/nbs/testfiles/LunarLander/LunarLander-c4d6de.properties'
+    suffixes = False
+    move = {}
+if env == "MountainCar":
+    config_file = f'C:/Users/{user}/Versioning/python/nbdev/pct/nbs/testfiles/MountainCar/MountainCar-cdf7cc.properties'
+    move = {'IV':[0, 0.05],'IP':[-0.6, 0.3],  'OL0C0sm':[-0.28, -0.2],'OL0C1sm':[0.28, -0.2], 'OL1C0sm':[0,-0.1], 'MountainCarContinuousV0':[-.7,-0.5], 'Action1ws':[-0.4,-0.3]}
+    # suffixes = False
 
 
 if test == "draw":
     result = PCTExamples.run_example(
         config_file=config_file,
-        print_summary=False,
+        print_summary=True,
         run_hierarchy=False,
         image_params={
-            'file': '/tmp/mountaincar_image.png',
-            # 'move': {'IV':[0, 0.6],'IP':[-0.6, 0.3],  'OL0C0sm':[-0.28, -0.2],'OL0C1sm':[0.28, -0.2], 'OL1C0sm':[0,-0.1], 'MountainCarContinuousV0':[-.7,-0.5], 'Action1ws':[-0.4,-0.3]},
-            'move': {'IV':[0, 0.05],'IP':[-0.6, 0.3],  'OL0C0sm':[-0.28, -0.2],'OL0C1sm':[0.28, -0.2], 'OL1C0sm':[0,-0.1], 'MountainCarContinuousV0':[-.7,-0.5], 'Action1ws':[-0.4,-0.3]},
+            'file': f'/tmp/{env}_image.png',
+            'move': move,
             'with_labels': True,
             'with_edge_labels': True,
-            # 'funcdata': True,
+            'funcdata': False,
             'font_size': 6,
             'node_size': 200
         },
+        suffixes=suffixes,
         get_model_details=True
     )
 
-#  
-
-
-    
 
 if test == "run":
     result = PCTExamples.run_example(
         config_file=config_file,
         print_summary=False,
         run_hierarchy=True,
-        steps=200,
-        verbose=True,
+        steps=500,
+        verbose=False,
         render=True,
         early_termination=True
     )
@@ -60,7 +66,7 @@ if test == "video":
         early_termination=True,
         video_params={
             'fps': 30,
-            'filename': '/tmp/mountaincar_simulation_video.mp4'
+            'filename': f'/tmp/{env}_simulation_video.mp4'
         }
     )
 
